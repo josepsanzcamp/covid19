2b2330d38960984231ec8830623915ef742817d7
Josep Sanz <josep.sanz@saltos.org>
2020-07-15 01:06:25 +0200

    Millores de rendiment a la funcio import_file

--- program.php
+++ program.php
@@ -3,7 +3,7 @@
 ini_set("memory_limit","1024M");
 ini_set("max_execution_time","3600");
 
-function import_file($file) {
+function import_file_old($file) {
 	$data=file_get_contents($file,null,null,0,1000);
 	$data=explode("\n",$data);
 	$sep="";
@@ -13,6 +13,35 @@ function import_file($file) {
 	$data=array();
 	while($row=fgetcsv($fd,0,$sep)) $data[]=$row;
 	fclose($fd);
+	return $data;
+}
+
+function import_file($file) {
+	$data=file($file,FILE_IGNORE_NEW_LINES);
+	$sep="";
+	if($sep=="" && strpos($data[0],";")!==false) $sep=";";
+	if($sep=="" && strpos($data[0],",")!==false) $sep=",";
+	foreach($data as $key=>$val) {
+		$val=explode($sep,$val);
+		$count=count($val);
+		for($i=0;$i<$count;$i++) {
+			if(substr($val[$i],0,1)=='"') {
+				$val[$i]=substr($val[$i],1);
+				for($j=$i;$j<$count;$j++) {
+					if(substr($val[$j],-1,1)=='"') {
+						$val[$j]=substr($val[$j],0,-1);
+						for($k=$i+1;$k<=$j;$k++) {
+							$val[$i].=$sep.$val[$k];
+							unset($val[$k]);
+						}
+						$i=$j;
+						break;
+					}
+				}
+			}
+		}
+		$data[$key]=array_values($val);
+	}
 	return $data;
 }
 

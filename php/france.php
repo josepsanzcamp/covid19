b97e3b48873f3c89504f25650a206c9fb7c29b9d
Josep Sanz <josep.sanz@saltos.org>
2020-11-17 21:34:53 +0100

    Arreglar algunes coses de rendiment

--- php/france.php
+++ php/france.php
@@ -2,6 +2,12 @@
 
 if(!file_exists("middle/dc_20xx_det.csv")) {
 	console_debug("middle/dc_20xx_det.csv");
+	passthru("zcat input/france/DC_20??_det.csv.gz | cut -d';' -f-3 | tr ';' '-' | uniq -c | gawk '{print $2\";\"$1}'| grep -v ADEC > middle/dc_20xx_det.csv");
+	console_debug();
+}
+
+if(!file_exists("middle/dc_20xx_det.csv")) {
+	console_debug("middle/dc_20xx_det.csv");
 	$files=glob("input/france/DC_20??_det.csv.gz");
 	sort($files);
 	$sumas=array();

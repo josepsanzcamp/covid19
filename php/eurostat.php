c139b7b01497dbb0974aa0917c17c6a1801e4420
Josep Sanz <josep.sanz@saltos.org>
2020-11-17 08:32:43 +0100

    Actualitzar les dades del Eurostat

--- php/eurostat.php
+++ php/eurostat.php
@@ -2,7 +2,9 @@
 
 if(!file_exists("middle/demo_r_mwk_ts.csv")) {
 	console_debug("middle/demo_r_mwk_ts.csv");
-	$buffer=get_file("input/eurostat/demo_r_mwk_ts.tsv.gz");
+	$files=glob("input/eurostat/demo_r_mwk_ts.*.tsv.gz");
+	rsort($files);
+	$buffer=get_file($files[0]);
 	$buffer=str_replace("\t",";",$buffer);
 	file_put_contents("middle/demo_r_mwk_ts.csv",$buffer);
 	console_debug();

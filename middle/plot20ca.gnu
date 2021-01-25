95e3311cb57d29ca1455098eace1951aa82bc57b
Josep Sanz <josep.sanz@saltos.org>
2021-01-25 19:22:14 +0100

    Fer millores al plot20, la idea es veure al xtic la data de quan s'ha publicat cada fitxer

--- middle/plot20ca.gnu
+++ middle/plot20ca.gnu
@@ -1,9 +1,9 @@
 set terminal png size 1200,600 enhanced font ',11'
-set title "20. Evolució de les dades del Índice Nacional de Defunciones"
+set title "20. Evolució de les dades de l'Índex Nacional de Defuncions"
 set grid
 set tmargin 3
 set rmargin 6
-set bmargin 3
+set bmargin 5
 set lmargin 6
 set auto x
 set yrange [0:500000]
@@ -11,11 +11,11 @@ set xdata time
 set timefmt '%Y-%m-%d'
 set format x '%Y-%m-%d'
 set xrange ['2019-02-01':'2021-02-01']
-set xtics '2019-04-01',86400*91,'2021-01-01'
+set xtics rotate by -45
 set ytic center rotate by 90
 set ytics 0,100000,400000
 set datafile separator ';'
 set colors classic
 set key at '2021-02-01',185000
 set output 'output/plot20ca.png'
-plot 'middle/plot20ca.csv' u 1:2 w lp ti col,'' u 1:3 w lp ti col,'' u 1:4 w lp ti col,'' u 1:5 w lp ti col
+plot 'middle/plot20ca.csv' u 1:2:xtic(1) w lp ti col,'' u 1:3 w lp ti col,'' u 1:4 w lp ti col,'' u 1:5 w lp ti col
--- php/plot20.php
+++ php/plot20.php
@@ -18,7 +18,7 @@ if(!file_exists("output/plot20${lang}.png")) {
 		"set grid",
 		"set tmargin 3",
 		"set rmargin 6",
-		"set bmargin 3",
+		"set bmargin 5",
 		"set lmargin 6",
 		"set auto x",
 		"set yrange [0:500000]",
@@ -26,14 +26,14 @@ if(!file_exists("output/plot20${lang}.png")) {
 		"set timefmt '%Y-%m-%d'",
 		"set format x '%Y-%m-%d'",
 		"set xrange ['2019-02-01':'2021-02-01']",
-		"set xtics '2019-04-01',86400*91,'2021-01-01'",
+		"set xtics rotate by -45",
 		"set ytic center rotate by 90",
 		"set ytics 0,100000,400000",
 		"set datafile separator ';'",
 		"set colors classic",
 		"set key at '2021-02-01',185000",
 		"set output 'output/plot20${lang}.png'",
-		"plot 'middle/plot20${lang}.csv' u 1:2 w lp ti col,'' u 1:3 w lp ti col,'' u 1:4 w lp ti col,'' u 1:5 w lp ti col",
+		"plot 'middle/plot20${lang}.csv' u 1:2:xtic(1) w lp ti col,'' u 1:3 w lp ti col,'' u 1:4 w lp ti col,'' u 1:5 w lp ti col",
 	))."\n";
 	file_put_contents("middle/plot20${lang}.gnu",$gnuplot);
 	passthru("gnuplot middle/plot20${lang}.gnu 2>&1");

5d78e9f2271ba284851dc11faca4f21139d2b20e
Josep Sanz <josep.sanz@saltos.org>
2021-02-01 08:35:07 +0100

    Moure el codi del index.html a un template, idea copiada del repo de la gralla

--- php/index.php
+++ php/index.php
@@ -2,29 +2,7 @@
 
 if(!file_exists("index.html")) {
 	console_debug("index.html");
-	$html=implode("\n",array(
-		"<!DOCTYPE html>",
-		"<html>",
-		"<head>",
-		"</head>",
-		"<body>",
-		"<script>",
-		"var lang=navigator.language || navigator.systemLanguage;",
-		"lang=lang.toLowerCase();",
-		"lang=lang.substr(0,2);",
-		"document.cookie.split(';').forEach(function(val,key) {",
-		"	val=val.trim().split('=');",
-		"	if(val[0]='lang') lang=val[1];",
-		"});",
-		"var hash=window.location.hash;",
-		"if(lang=='ca') window.location.href='index.ca.html'+hash;",
-		"else if(lang=='es') window.location.href='index.es.html'+hash;",
-		"else if(lang=='en') window.location.href='index.en.html'+hash;",
-		"else window.location.href='index.ca.html'+hash;",
-		"</script>",
-		"</body>",
-		"</html>",
-	))."\n";
+	$html=file_get_contents("template/index.js");
 	$html=html_minify($html);
 	file_put_contents("index.html",$html);
 	unset($html);

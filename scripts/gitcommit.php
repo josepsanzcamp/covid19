03a41bee529ab4945e95b7352d3a36fea811d6c6
Josep Sanz <josep.sanz@saltos.org>
2021-06-15 00:03:35 +0200

    Actualitzar/afegir les dades de momo i portugal

---
+++ scripts/gitcommit.php
@@ -0,0 +1,17 @@
+<?php
+ob_start();
+passthru("git status | grep input | cut -d/ -f2");
+$items=explode("\n",trim(ob_get_clean()));
+$items=array_diff($items,array(""));
+$total=count($items);
+if(!$total) {
+	die("Nothing to commit\n");
+}
+if($total>1) {
+	$items[$total-2]=$items[$total-2]." i ".$items[$total-1];
+	unset($items[$total-1]);
+}
+$items=implode(", ",$items);
+$message="Actualitzar/afegir les dades de ".$items;
+passthru('git commit -m "'.$message.'" -e');
+?>

9dec13f3d6847cc1624f80c7095cedc11b019a12
Josep Sanz <josep.sanz@saltos.org>
2021-12-11 07:19:45 +0100

    Actualitzar/afegir les dades de eurostat, france, momo i portugal

--- php/france.php
+++ php/france.php
@@ -5,6 +5,7 @@ if (!file_exists("middle/dc_20xx_det.csv")) {
     passthru("zcat input/france/DC_20*.csv.gz |
         cut -d';' -f-3 |
         tr ';' '-' |
+        sort |
         uniq -c |
         gawk '{print $2\";\"$1}'|
         grep -v ADEC > middle/dc_20xx_det.csv");

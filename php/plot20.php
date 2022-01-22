ac46e9626effc474a8f31c2fe5b41aaf1a8f9f12
Josep Sanz <josep.sanz@saltos.org>
2022-01-22 11:34:49 +0100

    Actualitzar/afegir les dades de eurostat, indef, momo i portugal

--- php/plot20.php
+++ php/plot20.php
@@ -25,30 +25,32 @@ if (!file_exists("output/plot20${lang}1.png")) {
         "set xdata time",
         "set timefmt '%Y-%m-%d'",
         "set format x '%Y-%m-%d'",
-        "set xtics rotate by -70",
+        "set xtics rotate by -60",
         "set xtics font ',10'",
         "set ytic center rotate by 90",
         "set ytics 0,100000,400000",
         "set datafile separator '" . SEPARADOR . "'",
         "set colors classic",
-        "set key at '2016-01-01',130000",
-        "set xrange ['2013-01-01':'2016-01-01']",
+        "set key at '2017-01-01',130000",
+        "set xrange ['2014-01-01':'2017-01-01']",
         "set output 'output/plot20${lang}1.png'",
         "plot 'middle/plot20${lang}.csv' u 1:2:xtic(1) w lp ti col,\
             '' u 1:3 w lp ti col,\
             '' u 1:4 w lp ti col,\
             '' u 1:5 w lp ti col,\
-            '' u 1:6 w lp ti col",
-        "set key at '2019-01-01',130000",
-        "set xrange ['2016-01-01':'2019-01-01']",
+            '' u 1:6 w lp ti col,\
+            '' u 1:7 w lp ti col",
+        "set key at '2020-01-01',130000",
+        "set xrange ['2017-01-01':'2020-01-01']",
         "set output 'output/plot20${lang}2.png'",
         "plot 'middle/plot20${lang}.csv' u 1:5:xtic(1) w lp ti col,\
             '' u 1:6 w lp ti col,\
             '' u 1:7 w lp ti col,\
             '' u 1:8 w lp ti col,\
-            '' u 1:9 w lp ti col",
-        "set key at '2022-01-01',130000",
-        "set xrange ['2019-01-01':'2022-01-01']",
+            '' u 1:9 w lp ti col,\
+            '' u 1:10 w lp ti col",
+        "set key at '2023-01-01',130000",
+        "set xrange ['2020-01-01':'2023-01-01']",
         "set output 'output/plot20${lang}3.png'",
         "plot 'middle/plot20${lang}.csv' u 1:8:xtic(1) w lp ti col,\
             '' u 1:9 w lp ti col,\

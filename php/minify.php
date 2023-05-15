bd0d6710d35bf9de59f2c8c8a51ea830c453509e
Josep Sanz <josep.sanz@saltos.org>
2023-05-15 14:58:09 +0200

    Actualitzer llibreria propia minify

--- php/minify.php
+++ php/minify.php
@@ -2,6 +2,13 @@
 
 function html_minify($data)
 {
+    $data = explode("\n", $data);
+    foreach ($data as $key => $val) {
+        if (substr(trim($val), 0, 4) == "<!--" && substr(trim($val), -3, 3) == "-->") {
+            unset($data[$key]);
+        }
+    }
+    $data = implode("\n", $data);
     $data = str_replace(array("\n","\r","\t"), " ", $data);
     do {
         $data = str_replace("  ", " ", $data, $count);
@@ -17,7 +24,7 @@ function css_minify($data)
     do {
         $data = str_replace("  ", " ", $data, $count);
     } while ($count);
-    foreach (array(":",";","{","}",",") as $temp) {
+    foreach (array(";","{","}",",") as $temp) {
         $data = str_replace(array(" " . $temp . " "," " . $temp,$temp . " "), $temp, $data);
     }
     $data = trim($data);
@@ -76,7 +83,7 @@ function html_minify2($data)
     // BUSCAR CSS
     $css = array();
     $cad1 = '<link href="';
-    $cad2 = '" rel="stylesheet">';
+    $cad2 = '" rel="stylesheet" />';
     $len1 = strlen($cad1);
     $len2 = strlen($cad2);
     $pos1 = strpos($data, $cad1);
@@ -96,7 +103,7 @@ function html_minify2($data)
 
 function css_minify2($data, $css)
 {
-    $data = str_replace("</head>", '<link href="' . $css . '" rel="stylesheet">' . "</head>", $data);
+    $data = str_replace("</head>", '<link href="' . $css . '" rel="stylesheet" />' . "</head>", $data);
     return $data;
 }
 
